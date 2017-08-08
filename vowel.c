495cf0e4ae466a350e3c189b4be9cade5776cc4d
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2017-08-07 22:19:44 -1200

    vowel.c
    vv

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char vowels;
+printf("enter the character:");
+scanf("%c",&vowels";
+if(char=vowels)
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is not a vowel");
+}}
