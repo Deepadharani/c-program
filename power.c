931ecbed695b2d4a22722dca166472d81c51725c
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2017-08-29 15:43:49 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,m;
+long long result=1;
+printf("enter the value of m and n:");
+scanf("%d,%d",&m,&n);
+while(m!=o)
+{
+result*=n;
+--m;
+}
+printf("the value is",result);
+return 0;
+}
