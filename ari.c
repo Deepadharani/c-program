1bc93245d5e53518d3f9620d7f3c88382b16381e
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-10 16:24:57 +0530

    Create ari.c

---
+++ ari.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c
+clrscr();
+printf("enter the value for a;");
+scanf("%d",&a);
+printf(:enter the value for b:");
+scanf("&d",&b);
+c=a+b;
+printf("the value of c is:");
+scanf("&d",&c);
+getch();
+}
