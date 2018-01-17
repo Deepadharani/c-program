698f8de512494dd651c34898825812a027c7b8ee
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-17 16:30:18 +0530

    Create odd2.c

---
+++ odd2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int ,ain()
+{
+int a,b,c;
+printf("enter the number:");
+scanf(%d%d,&m,&n);
+printf("all odd number from %d to %d are\n",m,n);
+for(a=0;a<b;a++)
+
