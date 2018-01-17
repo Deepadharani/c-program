52d0df819764a031c6e6c8f44a73aedd45f080f8
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-17 16:00:23 +0530

    Create even btw.c

---
+++ even btw.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int counter;
+printf("enter the number 1 or 2");
+counter =2;
+while(counter<=2)
+{
+printf("%d",counter);
+counter=counter+2;
+}
+return 0;
+}
