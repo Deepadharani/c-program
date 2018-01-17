ffc9e8f8aeb47cf6f7df09619a666785aa5314ff
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-17 16:47:55 +0530

    Create maxi.c

---
+++ maxi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int x[20],n,max,i;
+printf("enter n:");
+scanf("%D",&n);
+printf("enter array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+max=a[100];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("max=%d",max:");
+}
