b407373c73a7db9a99c686c401e45f2e321a7c2b
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-17 16:51:44 +0530

    Create mini.c

---
+++ mini.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int x[20],min,i;
+printf("enter n:");
+scanf("%d",&n);
+printf("enter array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>min)
+{
+min=a[i];
+}
+}
+printf("max=%d",max:");
+}
