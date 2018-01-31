c8b8d08fbae47dfd379234584e9755ca116538e1
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-31 16:10:45 +0530

    Create numbernumeric.c

---
+++ numbernumeric.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str[20];
+int i,count=0;
+printf("enter the string");
+scanf("S",&str[i]);
+for(i=0;str[i]!=null;i++)
+{
+if(str[i]>=1||str[i]<=9)
+{
+count++;
+}
+printf("%d",count);
+}
