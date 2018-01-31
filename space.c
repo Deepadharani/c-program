919cb2fec2a110c2d1429ac61049dcc44c24acaf
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-01-31 15:58:05 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char word[100];
+int spaces=0,i;
+printf("entyer the line of teaxt: \n");
+if(word[i]=='^~')
+{
+spaces ++;
+}
+printf("number of white space %d",spaces);
+}
