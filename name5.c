8f32ffb6cbfc62b52d50e843d44349a6370e3a74
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-02 16:49:01 +0530

    Create name5.c

---
+++ name5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char=name;
+int=num;
+printf("enter the name:");
+scanf("%s",&name);
+printf("enter the num:");
+scanf("%d",&num);
+for(num=o;num<=5;num++)
+{
+printf("deepa");
+}
+return 0;
+}
