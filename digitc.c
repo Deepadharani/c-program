87f9732a10fee253b154d88bb4c0926abc17bc9b
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-05 16:28:13 +0530

    Create digitc.c

---
+++ digitc.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num,rem,count=0;
+printf("enter the number:");
+scanf("%d",num);
+while(num>0)
+{
+num=num/10;
+coun++;
+}
+printf("number of digit is =%d",count);
+return 0;
+}
+
+
