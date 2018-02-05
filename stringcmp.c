16d8c24a8d20a42378aaad5898633a461385497a
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-05 15:47:06 +0530

    Create stringcmp.c

---
+++ stringcmp.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str1[20,str2[20];
+int compare;
+printf("enter the first string:");
+scanf("%s",&str1);
+printf("enter  the second string:");
+scanf("%s",&str2);
+compare=stringtcompare(str1,str2);
+if(compare==1)
+{
+printf("both are equal:");
+}
+else
+{
+printf("not eqal"):
+return 0;
+}
+
