04e353a0891fe04be309946c38bc5abe44550d0f
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-07 16:16:29 +0530

    Create powerof2.c

---
+++ powerof2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,m;
+printf("enter the numeber:");
+scan("%d",&n);
+m=(n&(n-1)))
+if(m==0)
+{
+printf(" of 02");
+}
+else
+{
+printf("not of of 2");
+}
+return 0;
+}
