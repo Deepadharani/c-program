73d1c47cd3f36d8838cf4edca40719383bc245f4
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-09 16:01:10 +0530

    Update product.c

--- product.c
+++ product.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
+int a,b;
+int c;
 printf("enter the a value");
 scanf("%d",&a);
 printf("enter the b value");
 scanf("%d",&b);
 c=a*b;
+printf("%d",c);
 if(c=%2)
 {
 printf("even");
