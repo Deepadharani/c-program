bed8661114fd3f8de119c538f0279ac9408ff0fb
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-09 15:54:11 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the a value");
+scanf("%d",&a);
+printf("enter the b value");
+scanf("%d",&b);
+c=a*b;
+if(c=%2)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
