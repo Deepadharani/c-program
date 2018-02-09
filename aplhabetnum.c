e6a10e7209a256efaefb54cd61f578bd4c941388
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-09 16:34:03 +0530

    Create aplhabetnum.c

---
+++ aplhabetnum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+int i;
+printf("enter value");
+scanf("%c",a);
+if(((a[i]>='a' && a[i]<='z' )|| (a[i]>='A' && a[i]<='Z')) || (a[i]>='0' && a[i]<='100'))
+{
+printf("yes")
+{
+else
+{
+printf("no");
+}
+return 0;
+}
