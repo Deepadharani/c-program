9581d105e4998d4fcd2b9ce4849b9b0bdd4299f0
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-23 16:13:42 +0530

    Create greatpower.c

---
+++ greatpower.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+int n,i,c=0,m=1,p=2
+printf("enter the integer");
+scanf("%d",&n);
+while(n!=1)
+{
+n=n/2;
+c++;
+}
+for(i=0;i<c+1;i++)
+{
+m=m*p;
+}
+printf("%d",m);
+return 0;
+}
