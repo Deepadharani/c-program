e2155a3f41d84fd15b4f44ddc2820a4c41e41752
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-02-23 15:51:26 +0530

    Create evendifference.c

---
+++ evendifference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+
+    printf("Enter an integer: ");
+    scanf("%d,%d", &a,&b);
+     c=a-b;
+     printf("%d",c);
+    if(number % 2 == 0)
+    {
+   printf("%d is even.",c) ;
+    }
+    else
+    {
+   printf("%d is odd.",c);
+    }
+    return 0;
+}
