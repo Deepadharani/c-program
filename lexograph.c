6b8e04d88ccd92e6ce1b34c8c17b22b7c5d2ecda
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-03-05 16:38:39 +0530

    Create lexograph.c

---
+++ lexograph.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,j,k;
+char str[100],tem[100];
+printf("enter the string");
+scanf("%c[^\n]s",str);
+for(i=oi<str[i]!='\0';i++)
+{
+for(j=i+1;dtr[j]!='\0';j++)
+{
+if(dtr[i]>str[j])
+{
+temp[k]=str[i];
+str[i]=str[j];
+str[j]=temp[k];
+}
+return 0;
+}
