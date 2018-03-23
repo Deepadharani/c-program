737187beec873ed4ebd69b1bcb016273ee126c3a
Deepadharani <30749618+Deepadharani@users.noreply.github.com>
2018-03-23 16:06:06 +0530

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h> 
+void append(char *s, char c)
+{ 	
+int s1=strlen(d); 	
+d[s1]=c; 	d[s1+1]='\0';
+} 
+void main()
+{ 
+char d[10],c,a; 	
+printf("enter string"); 	
+scanf("%s",s);	 	c='.'; 	
+append(d, c); 	
+printf("%s",d); 
+}
